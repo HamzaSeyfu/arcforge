2e2617fb29db9662bcc555baf6ac87cdaae87357
HamzaSeyfu <165190579+HamzaSeyfu@users.noreply.github.com>
2026-09-20 13:37:20 +0200

    Add synthesis import regression test

--- tests/test_synthesis.py
+++ tests/test_synthesis.py
@@ -1,43 +1,15 @@
 from arcforge.synthesis import synthesis_candidate_grids
 
 
-def test_depth_two_crop_then_rotate():
+def test_synthesis_identity_family_runs_without_nameerror():
     task = {
         "train": [
-            {
-                "input": [
-                    [0, 0, 0, 0],
-                    [0, 1, 2, 0],
-                    [0, 3, 4, 0],
-                    [0, 0, 0, 0],
-                ],
-                "output": [[3, 1], [4, 2]],
-            }
+            {"input": [[1, 2], [3, 4]], "output": [[1, 2], [3, 4]]},
         ],
         "test": [
-            {
-                "input": [
-                    [0, 0, 0, 0],
-                    [0, 5, 6, 0],
-                    [0, 7, 8, 0],
-                    [0, 0, 0, 0],
-                ]
-            }
+            {"input": [[5, 6], [7, 8]]},
         ],
     }
-    pools = synthesis_candidate_grids(task, max_depth=2)
-    assert [[7, 5], [8, 6]] in pools[0]
-
-
-def test_final_color_map_after_geometry():
-    task = {
-        "train": [
-            {
-                "input": [[1, 0], [0, 1]],
-                "output": [[2, 0], [0, 2]],
-            }
-        ],
-        "test": [{"input": [[1, 1], [0, 1]]}],
-    }
-    pools = synthesis_candidate_grids(task, max_depth=1)
-    assert [[2, 2], [0, 2]] in pools[0]
+    pools = synthesis_candidate_grids(task, max_depth=1, max_programs=8)
+    assert pools
+    assert [[5, 6], [7, 8]] in pools[0]
